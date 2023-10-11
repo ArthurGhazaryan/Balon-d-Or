cd02f84130f166940345c8956b1f7c03f6660034
ArthurGhazaryan <82112268+ArthurGhazaryan@users.noreply.github.com>
2023-10-11 10:39:55 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// let str = 'abcdefghijklmnop';
+// let last = str.length - 1;
+// alert(last);
+
+// let a = 'Javascript';
+// let b = a.length-1
+// console.log(a[b]);
+// console.log(a[a.length-2]);
+
+// let num = 47;
+// num += 7;
+// num -=18
+// num *= 10
+// num /=15
+// console.log(num);
+
+// let num1 = 0;
+// let num2 = num1++;
+// alert(num2);
+// alert(num1);
+
+// document.write(256456106);
