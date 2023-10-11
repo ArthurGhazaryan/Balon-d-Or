1d2707bfeeebe6a83da7e93c213a2ba133ba0b6c
ArthurGhazaryan <82112268+ArthurGhazaryan@users.noreply.github.com>
2023-10-11 13:33:08 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
-// let str = 'abcdefghijklmnop';
-// let last = str.length - 1;
-// alert(last);
-
-// let a = 'Javascript';
-// let b = a.length-1
-// console.log(a[b]);
-// console.log(a[a.length-2]);
-
-// let num = 47;
-// num += 7;
-// num -=18
-// num *= 10
-// num /=15
-// console.log(num);
-
-// let num1 = 0;
-// let num2 = num1++;
-// alert(num2);
-// alert(num1);
-
-// document.write(256456106);
+// let dom = "Gryazi"
+// if (dom === "Soloma") {
+//     console.log("3 raza dunut")
+// } else if (dom === "Vetki") {
+//     console.log("6 raz dunut")
+// } else if (dom === "Kamen") {
+//     console.log("Takoy dom ne sdut")
+// } else {
+//     console.log("Kakoy dom ne sduli")
+// }
